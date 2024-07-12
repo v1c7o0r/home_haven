b04d8111b809e29070ab0fcafe1b0ab9aa4922d5
victor <wahinyavictor818@gmail.com>
2024-07-12 16:46:05 +0300

    initial commit

--- server/models.py
+++ server/models.py
@@ -17,6 +17,3 @@ class User(db.Model):
 
     bookings = db.relationship('Booking', back_populates='user', cascade='all, delete-orphan')
     reviews = db.relationship('Review', back_populates='user', cascade='all, delete-orphan')
-
-    def __repr__(self):
-        return f'<User {self.username}>'
